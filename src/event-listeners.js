1baaa2b48c5fe35d9babbb6bb24a94db6be222a5
Adrian Lara <adrianblara@gmail.com>
2018-02-12 11:37:48 -0700

    refactor parsing of text into object

--- src/event-listeners.js
+++ src/event-listeners.js
@@ -5,18 +5,22 @@ import { postWords } from './fetch-requests'
 function breakDown() {
   $('button').on('click', function() {
     let text = document.getElementsByTagName('textarea')[0].value.toLowerCase()
-    let wordCounts = text.split(' ').reduce(function(allWords, word) {
-      if (allWords[word]) {
-        allWords[word]['value']++
-      } else {
-        (allWords[word] = { value: 1 })
-      }
-      return allWords
-    }, {})
+    let wordCounts = countWords(text)
 
     renderParagraph(wordCounts)
     postWords(wordCounts)
   })
+}
+
+function countWords(text) {
+  return text.split(' ').reduce(function(allWords, word) {
+    if (allWords[word]) {
+      allWords[word]['value']++
+    } else {
+      (allWords[word] = { value: 1 })
+    }
+    return allWords
+  }, {})
 }
 
 function returnBreakDown() {
